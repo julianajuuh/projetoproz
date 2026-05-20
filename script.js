539f328ca7ca0223e9d7e2bbc7be7b046e6f6231
julianafs <julianafonsecaribeirodasilva@gmail.com>
2026-05-20 20:28:58 -0300

    atual3

--- script.js
+++ script.js
@@ -9,16 +9,35 @@ const acoes = [
 
 const container = document.getElementById('container-acoes');
 if (container) {
-    for(let i = 0; 0< acoes.length; i++) {
+    for(let i = 0; 0 < acoes.length; i++) {
     const item = acoes[i]
     const div = document.createElement('div');
     div.className = 'acao-item';
     div.innerHTML = `
-    <div class = "acao-icone"> ${item-icone} </div
-    <p class = "acao-texto"> ${item-texto} </p>
+    <div class = "acao-icone"> ${item.icone} </div>
+    <p class = "acao-texto"> ${item.texto} </p>
     `;
 
     container.appendChild(div)
     }
+}
 
+const form = document.getElementById('formulario-adesao')
+const msg = document.getElementById('mensagem-retorno')
+
+if(form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        if( form.checkValidity()) {
+            const nome = document.getElementById('none').ariaValueMax;
+            msg.textContent = `✅ Sucesso, ${nome}! Adesão registrada.`
+            msg.className = 'mensagem-retorno sucesso';
+            form.reset()
+        } else {
+            msg.textContent = `❌ Preencha os campos corretamente.`;
+            msg.className = 'mensagem-retorno erro';
+            msg.style.display = 'block'
+        }
+    })
 }
