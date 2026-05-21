d2aa4f173d655f1e6df4260d75c13b7538136693
julianafs <julianafonsecaribeirodasilva@gmail.com>
2026-05-21 17:56:52 -0300

    atual4

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const acoes = [
-    {icone: '🚰', texto: 'Feche bem a torneira enquanto não estiver usando' },
-    {icone: '🔔', texto: 'Avise logo se perceber um vazamento' },
-    {icone: '♻️', texto: 'Reutilize agua de limpeza quando possivel' },
-    {icone: '🥤', texto: 'Use copo ou garrafa reutilizavel' },
-    {icone: '💬', texto: 'Conte para seus colegas essas dicas' },
-    {icone: '🌍', texto: 'Multiplique - cada pessoa influencia outtras 5' }
+    { icone: '🚰', texto: 'Feche bem a torneira quando não estiver usando' },
+    { icone: '🔔', texto: 'Avise logo se perceber um vazamento' },
+    { icone: '♻️', texto: 'Reutilize água de limpeza quando possível' },
+    { icone: '🥤', texto: 'Use copo ou garrafa reutilizável' },
+    { icone: '💬', texto: 'Conte para seus colegas sobre essas dicas' },
+    { icone: '🌍', texto: 'Multiplique — cada pessoa influencia outras 5' }
 ]
 
 const container = document.getElementById('container-acoes');
-if (container) {
-    for(let i = 0; 0 < acoes.length; i++) {
-    const item = acoes[i]
-    const div = document.createElement('div');
-    div.className = 'acao-item';
-    div.innerHTML = `
-    <div class = "acao-icone"> ${item.icone} </div>
-    <p class = "acao-texto"> ${item.texto} </p>
-    `;
-
-    container.appendChild(div)
+if(container) {
+    for(let i = 0; i< acoes.length; i++) {
+        const item = acoes[i]
+        const div = document.createElement('div');
+        div.className = 'acao-item';
+        div.innerHTML = `
+        <div class = "acao-icone"> ${item.icone} </div>
+        <p class = "acao-texto"> ${item.texto}</p>
+        `;
+        container.appendChild(div)
     }
 }
 
@@ -28,14 +27,14 @@ const msg = document.getElementById('mensagem-retorno')
 if(form) {
     form.addEventListener('submit', (e) => {
         e.preventDefault()
-
+        
         if( form.checkValidity()) {
-            const nome = document.getElementById('none').value;
+            const nome = document.getElementById('nome').value;
             msg.textContent = `✅ Sucesso, ${nome}! Adesão registrada.`
             msg.className = 'mensagem-retorno sucesso';
             form.reset()
         } else {
-            msg.textContent = `❌ Preencha os campos corretamente.`;
+            msg.textContent = '❌ Preencha os campos corretamente.';
             msg.className = 'mensagem-retorno erro';
             msg.style.display = 'block'
         }
